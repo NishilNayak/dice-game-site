65bd617920711b371a21878c6c2478c20810089c
Nishil <nishilnayak2222@gmail.com>
2023-01-15 21:25:31 +0530

    solved the refresh issue by adding window.performance.navigation.type

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+if (window.performance.navigation.type === 1) {
+
 var randomNum1 = Math.floor(Math.random() * 6)+1;
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
 var randomnNum2 = Math.floor(Math.random() * 6)+1;
@@ -6,11 +8,11 @@ document.querySelector(".img2").setAttribute("src", "images/dice" + randomnNum2 
 if(randomNum1>randomnNum2){
     document.querySelector("h1").textContent=" ✌️ Player 1 wins";
 };
-
 if(randomNum1<randomnNum2){
     document.querySelector("h1").textContent="Player 2 wins ✌️ ";
 };
 
 if(randomNum1==randomnNum2){
     document.querySelector("h1").textContent="Draw";
+};
 };
