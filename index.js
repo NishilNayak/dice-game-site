57dd6f758d75fda9c0e9e56832391c792f5aa322
Nishil <nishilnayak2222@gmail.com>
2023-01-15 20:59:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNum1 = Math.floor(Math.random() * 6)+1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+var randomnNum2 = Math.floor(Math.random() * 6)+1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomnNum2 + ".png");
+
+if(randomNum1>randomnNum2){
+    document.querySelector("h1").textContent=" ✌️ Player 1 wins";
+};
+
+if(randomNum1<randomnNum2){
+    document.querySelector("h1").textContent="Player 2 wins ✌️ ";
+};
+
+if(randomNum1==randomnNum2){
+    document.querySelector("h1").textContent="Draw";
+};
