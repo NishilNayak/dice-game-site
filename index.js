2c7ab3dcdece9aac1fca8bd9a7ab86c28d694dbb
Nishil <nishilnayak2222@gmail.com>
2023-01-21 18:22:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-if (window.performance.navigation.type === 1) {
-
 var randomNum1 = Math.floor(Math.random() * 6)+1;
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
 var randomnNum2 = Math.floor(Math.random() * 6)+1;
@@ -8,11 +6,11 @@ document.querySelector(".img2").setAttribute("src", "images/dice" + randomnNum2 
 if(randomNum1>randomnNum2){
     document.querySelector("h1").textContent=" ✌️ Player 1 wins";
 };
+
 if(randomNum1<randomnNum2){
     document.querySelector("h1").textContent="Player 2 wins ✌️ ";
 };
 
 if(randomNum1==randomnNum2){
     document.querySelector("h1").textContent="Draw";
-};
 };
